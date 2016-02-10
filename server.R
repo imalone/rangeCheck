ce7ea29df928ea29604288dea92391cb352b70fa
Ian Malone <ibmalone@gmail.com>
2016-02-10 17:57:26 +0000

    Working checks, plots, downloader button.

--- server.R
+++ server.R
@@ -1,50 +1,85 @@
 library(shiny)
 
 #library(ggplot2)
-#library(rCharts)
+library(rCharts)
 
 require(reshape2)
 source('support.R')
+options(RCHART_WIDTH=600)
 
 refInput <- readInput('data/test1.csv')
 
 shinyServer(
-  function(input,output) {
-    output$odate <- renderPrint({input$date})
+  function(input,output, clientData, session) {
+    indata <- refInput
+    # Update seed automatically based on selections.
+    # Putting the update to as.integer first makes sure it can only
+    # be changed to an integer.
+    observe ({
+      updateNumericInput(session, "seed", value=as.integer(input$seed))
+    })
+    observe ({
+      c_which <- input$which
+      c_type <- input$type
+      c_date <- input$date
+      updateNumericInput(session, "seed", value= {
+        checkstring <- paste0(c_which,c_type)
+        jointseed(input$date, checkstring)
+      })
+    })
+
+    output$odate <- renderText({as.character(input$date)})
     
+    datatocheck <- reactive({
+      buildInput(indata,input$which)
+    })
+    checklist <- reactive({
+      set.seed(input$seed)
+      outliers(datatocheck(), input$type,
+               upper.tail=input$propupper,
+               lower.tail=input$proplower,
+               inliers=input$propinlier)
+    })
+    output$checktable <- DT::renderDataTable(
+      DT::datatable(checklist(), options=list(pageLength=10)))
     
+    plotChecks <- reactive({
+      plotdata <- datatocheck()
+      checkdata <- checklist()
+      plotdata$check <- ifelse (plotdata$label %in% checkdata$label,
+                                "check", "no check")
+      if (input$type == "vol") {
+        eq <- as.formula("volB ~ volA")
+        xlabel = paste(input$which, "volA/ml")
+        ylabel = paste(input$which, "volB/ml")
+      } else {
+        eq <- as.formula("change ~ volDelta")
+        xlabel = paste(input$which, "delta vol/ml")
+        ylabel = paste(input$which, "change/ml")
+      }
+      n1 <- nPlot(eq, group="check", type="scatterChart", data=plotdata)
+      n1$chart(color=c("blue","orangered"))
+      n1$chart(tooltipContent = "#! function(key,x, y, e) {
+          return e.point.label
+                } !#")
+      n1$xAxis(axisLabel=xlabel) ; n1$yAxis(axisLabel=ylabel)
+      irsub <- iris
+      names(irsub) <- gsub('\\.', '', names(irsub))
+      #nPlot(SepalWidth ~ SepalLength, color="Species", type="scatterChart", data=irsub)
+      n1
+    })
+    output$outplot <- renderChart2({plotChecks()})
     
     output$downloadData <- downloadHandler(
-      filename = function() { "sample.csv"},
+      filename = function() {
+        descrip <- paste("rangecheck",input$date,input$which,input$type,
+                         input$seed, sep="_")
+        paste0(descrip,".csv")
+      },
       content = function(file) {
-        write.csv(refInput, file)
+        write.csv(checklist(), file)
       }
     )
     
-    output$oseed <- reactive({
-      checkstring <- paste0(input$which,input$type)
-      jointseed(input$date, checkstring)
-    })
-    
-    if(0){
-    output$testPlot <- renderChart2({ # vs renderPlot
-      # #      ggplot(aes(x=Sepal.Length,y=Sepal.Width, colour=Species),
-      # #                                 data=iris) + geom_point()
-      irsub <- iris
-      names(irsub) <- gsub('\\.', '', names(irsub))
-      #      r1<-rPlot(Sepal.Width ~ Sepal.Length, color="Species", type="point", data=iris)
-      #r1<-rPlot(SepalWidth ~ SepalLength, color="Species", type="point", data=irsub)
-      r1<-nPlot(SepalWidth ~ SepalLength, color="Species", type="scatterChart", data=irsub)
-      r1$chart(tooltipContent = "#! function(key,x, y, e) {
-          return 'x: ' + x + 's: ' + e.point.Species
-                } !#")
-      #r1<-rPlot(SepalLength ~ SepalWidth | Species, data = irsub, color = 'Species', type = 'point')
-      return(r1)
-      #irsub<-iris
-      #names(irsub) = gsub("\\.", "", names(irsub))
-      #rPlot(SepalLength ~ SepalWidth | Species, data = irsub, color = 'Species', type = 'point')
-      
-    })
-    }
   }
 )
--- ui.R
+++ ui.R
@@ -1,20 +1,30 @@
 library(shiny)
+require(rCharts)
+
 shinyUI(pageWithSidebar(
-  headerPanel("So very shiny"),
+  headerPanel("Range checker demo"),
   sidebarPanel(
-    h3('Sidebar text'),
+    h3('Settings'),
     dateInput('date','Preparation date'),
     selectInput("which", label="Results to check",
                 choices = list("brain", "vent"), selected="brain"),
     selectInput("type", label="Measure type",
                 choices = list("change", "vol"), selected="change"),
-    p('Seed'), textOutput('oseed'),
-    downloadButton('downloadData','Download')
+    numericInput("proplower", label="Lower tail proportion",
+                 value=0.02, step=0.005, min=0, max=0.5),
+    numericInput("propupper", label="Upper tail proportion",
+                 value=0.02, step=0.005, min=0, max=0.5),
+    numericInput("propinlier", label="Inlier proportion",
+                 value=0.01, step=0.005, min=0, max=0.5),
+    numericInput("seed", label="Seed for checks", value=0,
+                 step=1, min = - .Machine$integer.max, max= .Machine$integer.max)
   ),
   mainPanel(
-    h3('Main Panel text'),
-    p('Date'), verbatimTextOutput('odate')
-    #plotOutput('testPlot')
-    #showOutput('testPlot','nvd3') # vs plotOutput
+    h3('Check data', textOutput('odate')),
+    downloadButton('downloadData','Download'),
+
+    showOutput('outplot','nvd3'),
+    
+    DT::dataTableOutput('checktable')
   )
 ))
